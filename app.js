ed49533a2f3f4ec7ac263b6da35cdab2886d42d1
Nastya <nastyarivz23@gmail.com>
2020-08-31 15:09:57 +0300

    fixed mistake

--- app.js
+++ app.js
@@ -7,19 +7,14 @@ for(let i = 0; i < addToCartButtons.length; i++) {
         productsCountEl.textContent = +productsCountEl.textContent + 1;
     })
 }
-addToCartButtons.addEventListener("click", function(e) {
-    let current = e.target;
-    console.log(current)
-    current.style.backgroundColor = "red"
-})
 
 let likeButtons = document.querySelectorAll(".like");
 
 
 for(let i = 0; i < likeButtons.length; i++){
     likeButtons[i].addEventListener("click", function() {
-                this.classList.toggle("liked")
-            })
+    this.classList.toggle("liked")
+    })
  }
 
 //  slider
